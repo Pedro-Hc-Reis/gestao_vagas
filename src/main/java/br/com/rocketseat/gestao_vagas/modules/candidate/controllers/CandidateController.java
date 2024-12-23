1888ac3ce047cbcf730b20e994b419843bf61d21
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-23 11:23:28 -0300

    Validando usuário já existente

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,9 +1,12 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate.controllers;
 
+import br.com.rocketseat.gestao_vagas.exceptions.UserFoundException;
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.rocketseat.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +20,13 @@ public class CandidateController {
     private CandidateRepository candidateRepository;
 
     @PostMapping ( "/" )
-    public CandidateEntity create ( @Valid @RequestBody CandidateEntity candidateEntity ) {
-        return this.candidateRepository.save ( candidateEntity );
+    public ResponseEntity<Object> create ( @Valid @RequestBody CandidateEntity candidateEntity ) {
+
+        this.candidateRepository.findByUsernameOrEmail ( candidateEntity.getUsername ( ) ,
+                candidateEntity.getEmail ( ) ).ifPresent ( user -> {
+            throw new UserFoundException ( );
+        } );
+
+        return new ResponseEntity<> ( this.candidateRepository.save ( candidateEntity ) , HttpStatus.OK );
     }
 }
