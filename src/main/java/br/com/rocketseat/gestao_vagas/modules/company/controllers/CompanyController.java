bce43a288e3345932121f3d1e0650bee0825efca
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-23 16:57:28 -0300

    Job Controller

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -2,6 +2,7 @@ package br.com.rocketseat.gestao_vagas.modules.company.controllers;
 
 import br.com.rocketseat.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.rocketseat.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class CompanyController {
     private CreateCompanyUseCase createCompanyUseCase;
 
     @PostMapping ( "/" )
-    public ResponseEntity<Object> create ( @RequestBody CompanyEntity companyEntity ) {
+    public ResponseEntity<Object> create ( @Valid @RequestBody CompanyEntity companyEntity ) {
         try {
             var result = this.createCompanyUseCase.execute ( companyEntity );
             return ResponseEntity.ok ( ).body ( result );
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,6 +1,7 @@
 package br.com.rocketseat.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -15,6 +16,8 @@ public class JobEntity {
     private UUID id;
     private String description;
     private String benefits;
+
+    @NotBlank ( message = "Esse campo é obrigatório" )
     private String level;
     @ManyToOne ()
     @JoinColumn ( name = "company_id", insertable = false, updatable = false )
