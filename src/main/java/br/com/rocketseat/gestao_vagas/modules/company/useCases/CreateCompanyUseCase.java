8ad92fcca5d65dc7f29fd4343643def2342b67bb
Pedro Reis <pedrohenriquecreis@gmail.com>
2024-12-24 08:58:23 -0300

    UseCase: AuthenticateCompany

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -4,12 +4,15 @@ import br.com.rocketseat.gestao_vagas.exceptions.UserFoundException;
 import br.com.rocketseat.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.rocketseat.gestao_vagas.modules.company.repositories.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public CompanyEntity execute ( CompanyEntity companyEntity ) {
         this.companyRepository
@@ -17,6 +20,10 @@ public class CreateCompanyUseCase {
                 .ifPresent ( user -> {
                     throw new UserFoundException ( );
                 } );
+
+        var password = passwordEncoder.encode ( companyEntity.getPassword ( ) );
+        companyEntity.setPassword ( password );
+
         return this.companyRepository.save ( companyEntity );
     }
 }
