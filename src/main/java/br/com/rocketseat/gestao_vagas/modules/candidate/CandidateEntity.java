931be3c385fa1c01f308455855bdb2636641bbaa
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-26 14:17:45 -0300

    Refatorando Documentação

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,10 +1,12 @@
 package br.com.rocketseat.gestao_vagas.modules.candidate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
@@ -20,16 +22,24 @@ public class CandidateEntity {
     @Id
     @GeneratedValue ( strategy = GenerationType.UUID )
     private UUID id;
+
+    @Schema ( example = "Daniel de Souza", requiredMode = Schema.RequiredMode.REQUIRED, description = "Nome do candidato" )
     private String name;
 
+    @NotBlank
     @Pattern ( regexp = "\\S+", message = "O campo [username] não deve conter espaço" )
+    @Schema ( example = "daniel", requiredMode = Schema.RequiredMode.REQUIRED, description = "Username do candidato" )
     private String username;
 
     @Email ( message = "O campo [email] deve conter um e-mail válido" )
+    @Schema ( example = "daniel@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED, description = "E-mail do candidato" )
     private String email;
 
     @Length ( min = 10, max = 100, message = "O campo [password] deve estar entre 10 e 100 caracteres" )
+    @Schema ( example = "admin@1234", minLength = 10, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED, description = "Senha do candidato" )
     private String password;
+
+    @Schema ( example = "Desenvolvedor Java", requiredMode = Schema.RequiredMode.REQUIRED, description = "Breve descrição do candidato" )
     private String description;
     private String curriculum;
 
--- src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -26,6 +26,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping ( "/candidate" )
+@Tag ( name = "Candidato", description = "Informações do candidato" )
 public class CandidateController {
 
     @Autowired
@@ -36,6 +37,13 @@ public class CandidateController {
     private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping ( "/" )
+    @Operation ( summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato" )
+    @ApiResponses ( {
+            @ApiResponse ( responseCode = "200", content = {
+                    @Content ( schema = @Schema ( implementation = CandidateEntity.class ) )
+            } ) ,
+            @ApiResponse ( responseCode = "400", description = "Usuário já existe" )
+    } )
     public ResponseEntity<Object> create ( @Valid @RequestBody CandidateEntity candidateEntity ) {
         try {
             var result = this.createCandidateUseCase.execute ( candidateEntity );
@@ -47,15 +55,14 @@ public class CandidateController {
 
     @GetMapping ( "/" )
     @PreAuthorize ( "hasRole('CANDIDATE')" )
-    @Tag(name = "Candidato", description = "Informações do candidato")
-    @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", content = {
-                    @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
-            }),
-            @ApiResponse(responseCode = "400", description = "User not found")
-    })
-    @SecurityRequirement(name = "jwt_auth")
+    @Operation ( summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato" )
+    @ApiResponses ( {
+            @ApiResponse ( responseCode = "200", content = {
+                    @Content ( schema = @Schema ( implementation = ProfileCandidateResponseDTO.class ) )
+            } ) ,
+            @ApiResponse ( responseCode = "400", description = "User not found" )
+    } )
+    @SecurityRequirement ( name = "jwt_auth" )
     public ResponseEntity<Object> get ( HttpServletRequest request ) {
         var idCandidate = request.getAttribute ( "candidate_id" );
         try {
@@ -69,7 +76,6 @@ public class CandidateController {
 
     @GetMapping ( "/job" )
     @PreAuthorize ( "hasRole('CANDIDATE')" )
-    @Tag ( name = "Candidato", description = "Informações do candidato" )
     @Operation ( summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro" )
     @SecurityRequirement ( name = "jwt_auth" )
     @ApiResponses ( {
