200937b8a17b603de55bcf2b9a617a3910009b0a
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-26 13:56:17 -0300

    Inserindo exemplos nas requisições

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,12 +1,16 @@
 package br.com.rocketseat.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
+    @Schema ( example = "Vaga para pessoa desenvolvedora júnior", requiredMode = Schema.RequiredMode.REQUIRED )
     private String description;
+    @Schema ( example = "GymPass, Plano de saúde", requiredMode = Schema.RequiredMode.REQUIRED )
     private String benefits;
+    @Schema ( example = "JUNIOR", requiredMode = Schema.RequiredMode.REQUIRED )
     @NotBlank ( message = "Esse campo é obrigatório" )
     private String level;
 }
