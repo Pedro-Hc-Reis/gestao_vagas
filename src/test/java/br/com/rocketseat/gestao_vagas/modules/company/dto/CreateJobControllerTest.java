a766179cb8d9fd059f34d07e82f6a052dca115fc
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-27 10:09:27 -0300

    JobController: Criando caso de teste com exceção

--- src/test/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobControllerTest.java
+++ src/test/java/br/com/rocketseat/gestao_vagas/modules/company/dto/CreateJobControllerTest.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.gestao_vagas.modules.company.dto;
 
+import br.com.rocketseat.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.rocketseat.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.rocketseat.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.rocketseat.gestao_vagas.utils.TestUtils;
@@ -17,6 +18,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith ( SpringRunner.class )
 @SpringBootTest ( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
@@ -59,5 +64,23 @@ public class CreateJobControllerTest {
                 )
                 .andExpect ( MockMvcResultMatchers.status ( ).isOk ( ) );
         System.out.println ( result );
+    }
+
+    @Test
+    public void should_not_be_able_to_create_a_new_job_if_company_not_found ( ) throws Exception {
+        var createdJobDTO = CreateJobDTO.builder ( )
+                .benefits ( "BENEFITS_TEST" )
+                .description ( "DESCRIPTION_TEST" )
+                .level ( "LEVEL_TEST" )
+                .build ( );
+        try {
+            mvc.perform ( MockMvcRequestBuilders.post ( "/company/job/" )
+                    .contentType ( MediaType.APPLICATION_JSON )
+                    .content ( TestUtils.objectToJson ( createdJobDTO ) )
+                    .header ( "Authorization" , TestUtils.generateToken ( UUID.randomUUID ( ) , "JAVAGAS_@123#" ) )
+            );
+        } catch (Exception e) {
+            assertThat ( e ).isInstanceOf ( CompanyNotFoundException.class );
+        }
     }
 }
