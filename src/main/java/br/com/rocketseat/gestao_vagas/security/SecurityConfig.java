e699785e4897c19d5607e67566f4e60c4e525f4a
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2024-12-27 15:23:02 -0300

    Inserindo Actuator

--- src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/security/SecurityConfig.java
@@ -15,10 +15,11 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] SWAGGER_LIST = {
+    private static final String[] PERMIT_ALL_LIST = {
             "/swagger-ui/**" ,
             "/v3/api-docs/**" ,
             "/swagger-resource/**" ,
+            "/actuator/**"
     };
     @Autowired
     private SecurityCompanyFilter securityCompanyFilter;
@@ -33,7 +34,7 @@ public class SecurityConfig {
                             .requestMatchers ( "/company/" ).permitAll ( )
                             .requestMatchers ( "/company/auth" ).permitAll ( )
                             .requestMatchers ( "/candidate/auth" ).permitAll ( )
-                            .requestMatchers ( SWAGGER_LIST ).permitAll ( );
+                            .requestMatchers ( PERMIT_ALL_LIST ).permitAll ( );
                     auth.anyRequest ( ).authenticated ( );
                 } )
                 .addFilterBefore ( securityCandidateFilter , BasicAuthenticationFilter.class )
