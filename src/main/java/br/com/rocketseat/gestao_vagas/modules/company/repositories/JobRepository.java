7b42ec25bbb6f4ed9367ccd31612917d2dbea8f7
Pedro-Hc-Reis <pedro.henrique@softrack.com.br>
2025-01-06 13:13:47 -0300

    Criando listagem das vagas

--- src/main/java/br/com/rocketseat/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/rocketseat/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -9,4 +9,6 @@ import java.util.UUID;
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
 
     List<JobEntity> findByDescriptionContainingIgnoreCase ( String title );
+
+    List<JobEntity> findByCompanyId ( UUID companyId );
 }
